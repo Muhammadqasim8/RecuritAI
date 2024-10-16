761a7ea2e696a7ac92f67c04b690cd4a13e5ac1a
Muhammad Qasim <qasimramzan78@gmail.com>
2024-10-16 10:16:22 +0500

    Fix the logo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom'; // Import useNavigate
 import { HiMenu, HiX } from 'react-icons/hi';
-import Button from './Button';
-import logo from '../assets/logo.svg';
+import logo from '../assets/logo1.svg'
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -72,15 +71,36 @@ const Header = () => {
           </div>
         </div>
         {isMenuOpen && (
-          <div className="lg:hidden">
-            <div className="flex flex-col gap-4 md:p-4">
-              <button 
-                onClick={() => navigate('/registration')} // Navigate to Registration page
-                className="bg-[#0f172a] hover:text-[#0f172a] hover:bg-transparent border-2 border-[#0f172a] px-4 py-2 rounded-full transition-colors duration-300 "
-              >
-                Get Started Now
-              </button>
-            </div>
+          <div className="lg:hidden flex flex-col items-center">
+            <ul className="flex flex-col text-base font-medium list-none gap-4 mt-4">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <a href="#">Features</a>
+              </li>
+              <li>
+                <a href="#">How it Works</a>
+              </li>
+              <li>
+                <a href="#">FAQ</a>
+              </li>
+              <li>
+                <a href="#">Contact us</a>
+              </li>
+            </ul>
+            <Link
+              to="/login"
+              className="text-[#0f172a] border-2 border-[#0f172a] mt-4 hover:bg-[#0f172a] hover:text-white px-4 py-2 rounded-full transition-colors duration-300"
+            >
+              Log In
+            </Link>
+            <button 
+              onClick={() => navigate('/registration')} // Navigate to Registration page
+              className="bg-[#0f172a] text-white mt-4 hover:text-[#0f172a] hover:bg-transparent border-2 border-[#0f172a] px-4 py-2 rounded-full transition-colors duration-300"
+            >
+              Get Started Now
+            </button>
           </div>
         )}
       </nav>
